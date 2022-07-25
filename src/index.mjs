30556c40810c0771b67b5f67b3cf47699e57fa0a
Nikhil S <nikhil@nsnnd.com>
2022-07-25 16:56:55 -0400

    did a lot; now parsing arrangement and sections and moving to chords

--- src/index.mjs
+++ src/index.mjs
@@ -1,16 +1,16 @@
 "use strict";
-
-import {ifError} from "assert";
+import fm from 'front-matter';
+import yaml from 'js-yaml';
 
 // todo
-// 0. get a leetsheet string
+// 0. get a leetsheet string X
 // 0a. validate it
-// 1. get frontmatter
-// 2. find ## structure and parse it as yaml
-// 3. map over resulting array
-// 4. for each item:
-// 4a. grab relevant section based on name
-// 4b. convert chords to a flat array of:
+// 1. get frontmatter X
+// 2. find ## arrangement and parse it as yaml
+// 3. find ## section and parse it as yaml
+// 4. for each section:
+// 4a. split out all chords using regex.
+// 4b. map over chords to create a flat array of:
 //      {
 //        chord: string,
 //        duration: string,
@@ -20,26 +20,82 @@ import {ifError} from "assert";
 // 5. append to master flat array of `song`
 
 const rgx = {
-    frontmatter: /^\-\-\-\n(.*)\-\-\-$/ms,
-    individualFM: /^(\w+)\:\s(.*)$/gm
+    //structure: /^\-\-\-\n(.*)\-\-\-$/ms,
+    //sections: /^(\w+)\:\s(.*)$/gm
+    arrangement: /^##\sarrangement\n([\W\w]+?)^##[^#\n]+/gms,
+    sections: /^##\ssections\n(.+)/gms
 };
 
 const getFrontmatter = function(sheet) {
-    var m = sheet.match(rgx.frontmatter);
-    if ( m ) {
-        return m[1]
-    } else {
-        console.error('no frontmatter');
+    try {
+        fm(sheet)
+        return fm(sheet).attributes
+    } catch(e) {
+        console.error("unable to parse frontmatter")
     }
 }
 
-const fmToJson = function(fmStr) {
-    // take the front matter and make key/values
+const getArrangement = function (sheet) {
+    let m = sheet.match(rgx.arrangement);
+    if (m) {
+        let arr = yaml.load(m[0]);
+        return arr;
+    } else {
+        console.error('no arrangement found');
+    }
+}
+
+const getSections = function (sheet) {
+    let m = sheet.match(rgx.sections);
+    if (m) {
+        console.log(m)
+        let sections = yaml.load(m[0]);
+        return sections;
+    } else {
+        console.error('no sections found');
+    }
+
+}
+
+const progressionToMeasures = function(progression) {
+    return progression
+        .replace(/\n/g, "")
+        .replace(/\s\|/g, "|")
+        .replace(/\|\s/g, "|")
+        .split("|")
+        .filter(x => x);
+}
+
+const processChordProgression = function(section) {
+    let measures = progressionToMeasures(section);
+    console.log(measures)
+}
+
+const makeSong = function (sheet) {
+    let sections = getSections(sheet)
+    Object.keys(sections).reduce(function(arr, k) {
+        return [
+            ...arr,
+            processChordProgression(sections[k])
+        ]
+    }, [])
+    return "";
 }
 
 const parseSheet = function (sheet) {
-    console.log(getFrontmatter(sheet))
-    return 'asdf'
+    let meta = getFrontmatter(sheet);
+    let arrangement = getArrangement(sheet);
+    let song = makeSong(sheet);
+
+    let ls = {
+        meta: meta,
+        arrangement: arrangement,
+        song: song
+    };
+    console.log(ls)
+    return ls;
+    //let ls = fmToJson(fm);
+    //return ls;
 };
 
 export { parseSheet as parse }
--- test/index.test.js
+++ test/index.test.js
@@ -2,16 +2,30 @@ import test from 'tape';
 import fs from 'fs';
 import { parse } from '../src';
 
-test('read', (t) => {
+const expectedLS = {
+  title: "Purple Rain",
+  artist: "Prince",
+  tempo: 80,
+  time: "4/4"
+};
+
+test('get frontmatter', (t) => {
   t.plan(1);
 
   fs.readFile('./test/purplerain.txt', 'utf8', (err, txt) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    t.equal(parse(txt), 'asdf');
+    if (err) console.error(err);
+    t.deepEqual(parse(txt), expectedLS);
   });
 
 })
+
+//test('invalid frontmatter returns error', (t) => {
+  //t.plan(1);
+
+  //fs.readFile('./test/purplerain-bad.txt', 'utf8', (err, txt) => {
+    //if (err) console.error(err);
+    //t.error(parse(txt));
+  //});
+
+//})
 
